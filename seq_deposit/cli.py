baf0bf76e34f9954e09db37903a0a93b8946ec04
Joseph Yesselman <jyesselm@unl.edu>
2024-05-26 22:00:41 -0500

    validating assembly sequneces

--- seq_deposit/cli.py
+++ seq_deposit/cli.py
@@ -8,6 +8,7 @@ import click
 import pandas as pd
 import yaml
 import sys
+import glob
 from pathlib import Path
 from typing import List
 
@@ -16,7 +17,7 @@ from seq_tools import to_fasta, to_dna
 from gsheets.sheet import get_next_code, get_sequence_sheet, get_oligo_sheet
 
 from seq_deposit.logger import setup_logging, get_logger
-from seq_deposit.deposit import deposit_files
+from seq_deposit.deposit import deposit_files, deposit_rna_csv
 from seq_deposit.prepare import generate_dna_dataframe, generate_rna_dataframe
 
 from seq_deposit.gsheets import (
@@ -29,6 +30,62 @@ log = get_logger(__name__)
 
 
 # helper functions ############################################################
+
+
+def get_rna_dataframe_from_row(row, columns):
+    row_df = pd.DataFrame([row], columns=columns, index=[0])
+    row_df = row_df[["name", "rna_sequence", "rna_structure"]]
+    row_df.rename(
+        {"rna_sequence": "sequence", "rna_structure": "structure"},
+        axis=1,
+        inplace=True,
+    )
+    row_df = generate_rna_dataframe(row_df, "RNA")
+    return row_df
+
+
+def fasta_to_dataframe(fasta_file):
+    """
+    Reads a FASTA file and converts it into a DataFrame with columns 'name' and 'sequence'.
+
+    Parameters:
+    fasta_file (str): Path to the FASTA file.
+
+    Returns:
+    pd.DataFrame: DataFrame with columns 'name' and 'sequence'.
+    """
+    names = []
+    sequences = []
+    sequence = []
+
+    with open(fasta_file, "r") as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith(">"):
+                if sequence:
+                    sequences.append("".join(sequence))
+                    sequence = []
+                names.append(line[1:])
+            else:
+                sequence.append(line)
+        if sequence:
+            sequences.append("".join(sequence))
+
+    data = {"name": names, "sequence": sequences}
+    return pd.DataFrame(data)
+
+
+def validate_rna_csv(row, seq_path):
+    if not os.path.exists(f"{seq_path}/rna/{row['code']}.csv"):
+        log.warning(f"rna csv does not exist for {row['code']}")
+        return False
+    df_rna = pd.read_csv(f"{seq_path}/rna/{row['code']}.csv")
+    if row["type"] == "ASSEMBLY":
+        csv_row = df_rna.iloc[0]
+        if row["rna_sequence"] != csv_row["sequence"]:
+            log.warning(f"rna sequence does not match for {row['code']}")
+            return False
+        return True
 
 
 def setup(
@@ -305,6 +362,36 @@ def get_last_code():
     print(df_primers.iloc[-1])
 
 
+# TODO search by name size etc return csv with results
+@cli.command()
+@click.option("--code", default=None, help="code of the sequence")
+def get_sequence_info(code):
+    setup_logging()
+    df = get_sequence_sheet()
+    if code is not None:
+        df = df[df["code"] == code]
+        if len(df) == 0:
+            log.error(f"code {code} not found")
+            return
+    log.info("num of constructs %d", len(df))
+    seq_path = os.environ["SEQPATH"]
+    for i, row in df.iterrows():
+        if row["size"] == 1:
+            print("name: ", row["name"])
+            print("sequence: ", row["rna_sequence"])
+            print("structure: ", row["rna_structure"])
+        else:
+            df_rna = pd.read_csv(f"{seq_path}/rna/{row['code']}.csv")
+            rna_row = df_rna.iloc[0]
+            print("example sequence ->")
+            print("name: ", rna_row["name"])
+            print("rna sequence: ", rna_row["sequence"])
+            print("rna structure: ", rna_row["structure"])
+        print("code: ", row["code"])
+        print("type: ", row["type"])
+        print("-" * 40)
+
+
 # TODO takes a seq-desposit directory and actually transfers files!
 @cli.command()
 @click.option("--deposit-path", default=None, help="deposit path")
@@ -325,6 +412,46 @@ def deposit(deposit_path, force):
         return
     if deposit_path is None:
         pass
+
+
+@cli.command()
+@click.option("--debug", is_flag=True, help="debug mode")
+def check_sequence_sheet(debug):
+    setup_logging()
+    if not debug:
+        log.info("backing up sequence and oligo directory")
+        os.system("zip -r sequences_and_oligos.zip $SEQPATH")
+    df = get_sequence_sheet()
+    seq_path = os.environ["SEQPATH"]
+    for i, row in df.iterrows():
+        if row["type"] == "ASSEMBLY" and row["usuable"] != "NO":
+            if not validate_rna_csv(row, seq_path):
+                row_df = get_rna_dataframe_from_row(row, df.columns)
+                deposit_rna_csv(row_df, row["code"], seq_path)
+            # df_rna = pd.read_csv(f"{seq_path}/rna/{row['code']}.csv")
+            # df_dna = pd.read_csv(f"{seq_path}/dna/{row['code']}.csv")
+            # df_fasta = fasta_to_dataframe(f"{seq_path}/fastas/{row['code']}.fasta")
+
+
+@cli.command()
+def get_invalid_files():
+    """
+    remove files that are not in the sequence sheet
+    """
+    setup_logging()
+    df = get_sequence_sheet()
+    seq_path = os.environ["SEQPATH"]
+    rna_csvs = glob.glob(f"{seq_path}/rna/*.csv")
+    dna_csvs = glob.glob(f"{seq_path}/dna/*.csv")
+    fasta_files = glob.glob(f"{seq_path}/fastas/*.fasta")
+    files = rna_csvs + dna_csvs + fasta_files
+    f = open("README_REMOVE", "w")
+    for file in files:
+        code = Path(file).stem
+        if code not in df["code"].values:
+            log.info(f"code {code} not in sequence sheet")
+            f.write(f'rm "{file}"\n')
+    f.close()
 
 
 if __name__ == "__main__":
--- seq_deposit/deposit.py
+++ seq_deposit/deposit.py
@@ -21,42 +21,7 @@ from seq_deposit.logger import get_logger
 log = get_logger("deposit")
 
 
-def generate_dna_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    generates the dna dataframe
-    :param df:
-    :return: the dna dataframe
-    """
-    df_dna = df.copy()
-    if "name" not in df_dna.columns:
-        df_dna = get_default_names(df_dna)
-    df_dna = to_dna(df_dna)
-    df_dna = df_dna[["name", "sequence"]]
-    df_dna = get_length(df_dna)
-    df_dna = get_molecular_weight(df_dna, "DNA", True)
-    df_dna = get_extinction_coeff(df_dna, "DNA", True)
-    return df_dna
-
-
-def generate_rna_dataframe(df: pd.DataFrame, ignore_missing_t7: bool) -> pd.DataFrame:
-    """
-    generates the rna dataframe
-    :param df: the dataframe with sequences
-    :param ignore_missing_t7: if true, ignore missing t7 promoter
-    :return: the rna dataframe
-    """
-    df_rna = df.copy()
-    df_rna = transcribe(df_rna, ignore_missing_t7)
-    df_rna = df_rna[["name", "sequence", "structure"]]
-    df_rna = get_length(df_rna)
-    df_rna = get_molecular_weight(df_rna, "RNA", False)
-    df_rna = get_extinction_coeff(df_rna, "RNA", False)
-    return df_rna
-
-
-def deposit_dna_csv(
-    df: pd.DataFrame, code: str, deposit_path: str, dry_run: bool
-) -> None:
+def deposit_dna_csv(df: pd.DataFrame, code: str, deposit_path: str) -> None:
     """
     generates the dna dataframe
     :param df: the dataframe with sequences
@@ -64,33 +29,19 @@ def deposit_dna_csv(
     :param deposit_path: where to deposit the csv file
     :param dry_run: if true, don't actually deposit the file
     """
-    df = generate_dna_dataframe(df)
-    if not dry_run:
-        log.info(f"writing dna csv to {deposit_path}/dna/")
-        path = os.path.join(deposit_path, "dna", f"{code}.csv")
-        df.to_csv(path, index=False)
+    log.info(f"writing dna csv to {deposit_path}/dna/")
+    path = os.path.join(deposit_path, "dna", f"{code}.csv")
+    df.to_csv(path, index=False)
 
 
 def deposit_rna_csv(
     df: pd.DataFrame,
     code: str,
     deposit_path: str,
-    dry_run=False,
-    ignore_missing_t7=False,
 ) -> None:
-    """
-    deposits the rna csv
-    :param df: the dataframe with sequences
-    :param code: the construct code
-    :param deposit_path: where to deposit the csv file
-    :param dry_run: if true, don't actually deposit the file
-    :param ignore_missing_t7: if true, ignore missing t7 promoter
-    """
-    df = generate_rna_dataframe(df, ignore_missing_t7)
-    if not dry_run:
-        log.info(f"writing rna csv to {deposit_path}/rna/")
-        path = os.path.join(deposit_path, "rna", f"{code}.csv")
-        df.to_csv(path, index=False)
+    log.info(f"writing rna csv to {deposit_path}/rna/")
+    path = os.path.join(deposit_path, "rna", f"{code}.csv")
+    df.to_csv(path, index=False)
 
 
 def deposit_fasta_file(df: pd.DataFrame, code, deposit_path, dry_run=False) -> None:
